a3148d456b7cf2bfd88e11c77e6fc0e5a64bbb04
NIRAV2742 <niravsavaliya290@gmail.com>
2025-09-22 21:40:10 +0530

    Update mmr_allo_obj.cpp

--- mmr_allo_obj.cpp
+++ mmr_allo_obj.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-  return 0;
+
+class Student {
+public:
+    string name;
+    int age;
+
+    void display() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
+int main() {
+    // dynamically allocate object
+    Student *s = new Student;
+
+    s->name = "Nirav";
+    s->age = 20;
+
+    s->display();
+
+    delete s; // deallocate object
+    return 0;
 }
